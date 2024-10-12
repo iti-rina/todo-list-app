465165c32607fcba553e4c5585a6b35ce6801718
iti-rina <itigecheva.irina@gmail.com>
2024-10-13 00:06:32 +0700

    feat: add filtersReducer to the global store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { todosReducer } from '../features/addTodo/model';
+import { filtersReducer } from '../features/filterTodos/model';
 
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
+    filters: filtersReducer,
   },
 });
 
