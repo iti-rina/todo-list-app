9dc7d61f63df89d030065b5d52799d3a1caba712
iti-rina <itigecheva.irina@gmail.com>
2024-10-12 22:01:52 +0700

    feat: create AddTodoForm component; add handler to dispatch adding new task to state

---
+++ src/features/addTodo/ui/AddTodoForm.tsx
@@ -0,0 +1,40 @@
+import { useState, MouseEvent } from 'react';
+import { useDispatch } from 'react-redux';
+import { nanoid } from '@reduxjs/toolkit';
+import { addTodo } from '../model';
+
+const AddTodoForm = () => {
+  const dispatch = useDispatch();
+
+  const [inputText, setInputText] = useState('');
+
+  const handleTaskSave = (e: MouseEvent<HTMLButtonElement> ) => {
+    e.preventDefault();
+    if (!inputText) return;
+
+    const todoContent = inputText.trim();
+
+    const id = nanoid();
+    dispatch(addTodo({ id, title: todoContent, isSelected: false }));
+    setInputText('');
+  }
+
+  return (
+    <div>
+      <form>
+        <input 
+          type='text'
+          value={inputText}
+          onChange={e => setInputText(e.target.value)}
+          placeholder='Add new todo'
+          maxLength={180}
+        />
+        <button onClick={handleTaskSave}>
+          Добавить
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default AddTodoForm;
