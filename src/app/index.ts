58c989b5a0e687e4c8bc8194d8f1eaff6dfef029
iti-rina <itigecheva.irina@gmail.com>
2024-10-12 22:17:35 +0700

    fix: change imports

---
+++ src/app/index.ts
@@ -0,0 +1,6 @@
+import App from './App';
+import { store } from './store';
+import type { RootState } from './store';
+
+export { App, store };
+export type { RootState };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './app/App.tsx';
-import { store } from './app/store.ts';
+import { App, store } from './app';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
