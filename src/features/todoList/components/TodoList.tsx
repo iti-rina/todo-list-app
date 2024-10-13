9196bac3748a7c2905d2e14ec8ef89ad7c5cbd1e
iti-rina <itigecheva.irina@gmail.com>
2024-10-13 22:36:30 +0700

    feat(style): add styles for ul in TodoList

--- src/features/todoList/components/TodoList.tsx
+++ src/features/todoList/components/TodoList.tsx
@@ -7,7 +7,7 @@ const TodoList = () => {
 
 
   return (
-    <ul>
+    <ul className='flex-grow bg-white rounded-lg'>
       {allTodosIds.map(id => <TodoItem todoId={id} key={id} />)}
     </ul>
   );
