33487c9a1cabc3458f7594d3c943786d1bb0d654
iti-rina <itigecheva.irina@gmail.com>
2024-10-14 02:03:21 +0700

    chore: change mock-data titles for language consistency

--- src/features/todoList/model/todosSlice.ts
+++ src/features/todoList/model/todosSlice.ts
@@ -9,9 +9,9 @@ type Todo = {
 
 // Моковые данные, для удобства отладки
 const initialTodos: Todo[] = [
-  { id: 'edc39aa9-3eec-41ed-82dc-27ec8d6b9661', title: 'Убрать в комнате', isSelected: false },
-  { id: 'a65cf47c-0665-432a-8448-bb6f81a2a4e4', title: 'Сходить в магазин', isSelected: false },
-  { id: '4dac7fa3-60f2-4c6e-99a3-c2d94805d690', title: 'Погулять с собакой', isSelected: false },
+  { id: 'edc39aa9-3eec-41ed-82dc-27ec8d6b9661', title: 'Make an appointment to doctor', isSelected: false },
+  { id: 'a65cf47c-0665-432a-8448-bb6f81a2a4e4', title: 'Call Mom', isSelected: true },
+  { id: '4dac7fa3-60f2-4c6e-99a3-c2d94805d690', title: 'Order poducts from grocery store', isSelected: false },
 ];
 
 
