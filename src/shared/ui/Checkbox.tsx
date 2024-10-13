e671d769d18005e9a829d4164009fcf674deb9ca
iti-rina <itigecheva.irina@gmail.com>
2024-10-13 21:40:51 +0700

    feat: create custom checkbox component

---
+++ src/shared/ui/Checkbox.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+
+type CheckboxProps = {
+  checked: boolean;
+}
+const Checkbox: FC<CheckboxProps> = ({ checked }) => {
+
+  return (
+    <div>
+      <label>
+        <input
+          type='checkbox'
+          className='hidden'
+          tabIndex={0}
+        />
+      <span
+        className={`w-6 h-6 flex items-center justify-center border-2 rounded ${checked ? 'bg-blue-500 border-blue-500' : 'border-gray-400'}`}
+      >
+        {checked && (
+          <svg
+            className='w-4 h-4 text-white'
+            stroke='#fff'
+            fill='none'
+            viewBox='0 0 24 24'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M5 13l4 4L19 7'></path>
+          </svg>
+        )}
+      </span>
+      </label>
+  </div>
+  );
+}
+
+export default Checkbox;
