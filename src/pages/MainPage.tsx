aaab1ffad20109d988cf7380187eaf0c0dbc0687
iti-rina <itigecheva.irina@gmail.com>
2024-10-13 23:48:01 +0700

    feat(style): add styles fo MainPage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -4,16 +4,18 @@ import { Controllers } from '../widgets/ui';
 
 const MainPage = () => {
   return (
-    <>
-      <header>
-        <h1>TODOS</h1>
-      </header>
-      <main>
-        <AddTodoForm />
-        <TodoList />
-        <Controllers />
-      </main>
-    </>
+      <div className='w-screen min-h-screen flex flex-col px-10 mx-auto bg-gradient-to-br from-bg-1 via-bg-2 to-bg-3'>
+        <div className='max-w-xl mx-auto md:mx-auto w-full my-20 text-left flex flex-col gap-12'>
+          <header className='text-4xl font-bold text-white tracking-widest'>
+            <h1>TODOS</h1>
+          </header>
+          <main className='flex flex-col gap-12 bg-inherit rounded-lg'>
+            <AddTodoForm />
+            <TodoList />
+            <Controllers />
+          </main>
+        </div>
+      </div>
   );
 }
 
