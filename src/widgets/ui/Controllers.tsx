334ec2a07c293ebf5514aaffc2475af0b1a1b42e
iti-rina <itigecheva.irina@gmail.com>
2024-10-13 23:47:41 +0700

    fix: restructure component fo mobile devices

--- src/widgets/ui/Controllers.tsx
+++ src/widgets/ui/Controllers.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { ClearCompleted } from '../../features/clearCompleted/components';
 import { FiltersController } from '../../features/filterTodos/ui';
 import { useSelector } from 'react-redux';
@@ -6,13 +7,30 @@ import { selectActive } from '../../features/filterTodos/model';
 const Controllers = () => {
   const activeQuantity = useSelector(selectActive).length;
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const isHidden = windowWidth < 768;
+
   return (
-    <div>
-      <p>
-        {activeQuantity > 1 ? `${activeQuantity} items left` : `${activeQuantity} item left`} 
-      </p>
-      <FiltersController />
-      <ClearCompleted />
+    <div className='flex flex-col items-start'>
+      <div className='flex justify-between text-white w-full'>
+        <p>
+          {activeQuantity > 1 ? `${activeQuantity} items left` : `${activeQuantity} item left`} 
+        </p>
+        { !isHidden ? <FiltersController /> : null}
+        <ClearCompleted />
+      </div>
+      {isHidden ? <FiltersController /> : null}
     </div>
   );
 }
