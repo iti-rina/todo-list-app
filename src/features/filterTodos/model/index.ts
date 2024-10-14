afd799b7e1c736c29582ddae41b2ac9675dccf66
iti-rina <itigecheva.irina@gmail.com>
2024-10-14 14:50:10 +0700

    fix: import fixes

--- src/features/filterTodos/model/index.ts
+++ src/features/filterTodos/model/index.ts
@@ -1,3 +1,3 @@
-import filtersReducer, { selectStatus, statusFilters, selectFilteredTaskIds, selectCompleted, selectActive } from './filtersSlice';
+import filtersReducer, { selectStatus, statusFilters, selectFilteredTaskIds, selectCompleted, selectActive, statusFilterChanged } from './filtersSlice';
 
-export { filtersReducer, selectStatus, statusFilters, selectFilteredTaskIds, selectCompleted, selectActive };
+export { filtersReducer, selectStatus, statusFilters, selectFilteredTaskIds, selectCompleted, selectActive, statusFilterChanged };
